1f7a8e09c3e81b8b0dfcfa5c2f20437ab8899309
Aikel <aikelen.030904@gmail.com>
2024-10-01 01:47:59 +0300

    c

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,10 @@ public class Main {
         //1
         int savingMoney = 0;
         int month = 0;
-        while (savingMoney <= 2_459_000){
+        while (savingMoney < 2_459_000){
             savingMoney += 15_000;
-            month += 1;
-            System.out.println("Месяц "+month+" Сумма накоплений равна "+savingMoney+" рублей");
+            month++;
+            System.out.println("1) Месяц "+month+" Сумма накоплений равна "+savingMoney+" рублей");
 
         }
         //2
@@ -20,6 +20,7 @@ public class Main {
         for (int i = 10;i>=1;i--){
             System.out.print(i +" ");
         }
+        System.out.println(" ");
         //3
         int person = 12_000_000;
         double birthPerson = 17;
@@ -31,17 +32,28 @@ public class Main {
         for (int year = 1; year <=years; year++){
             int populationChange = (int) (person * changePerson / 1000);
             person += populationChange;
-            System.out.println("Год "+ year + ", численность населения "+ person);
+            System.out.println("3)Год "+ year + ", численность населения "+ person);
+        }
+        System.out.println(" ");
+        //4
+        int firstVklad = 15_000;
+        month = 0;
+        while (firstVklad <= 12_000_000){
+            firstVklad += (firstVklad * 7 / 100);
+            month++;
+            System.out.println("4)Месяц "+month+" количество денег "+ firstVklad);
+
+
         }
 
-        //4-5
-        int firstVklad = 15_000;
-        int Month = 0;
+        //5
+        firstVklad = 15_000;
+        month = 0;
         while (firstVklad <= 12_000_000){
             firstVklad += (firstVklad * 7 / 100);
             month++;
             if (month % 6 == 0){
-                System.out.println("месяц "+month+" количество денег " + firstVklad);
+                System.out.println("5)месяц "+month+" количество денег " + firstVklad);
             }
 
 
@@ -56,31 +68,25 @@ public class Main {
             firstVkladTwo += (firstVkladTwo * 7 / 100);
             ForEighteenMonth++;
             if (ForEighteenMonth % 6 == 0){
-                System.out.println("Месяц "+ForEighteenMonth+" количество накоплений "+firstVkladTwo);
+                System.out.println("6)Месяц "+ForEighteenMonth+" количество накоплений "+firstVkladTwo);
             }
         }
 
         //7
         int Friday = 5;
         while (Friday < 31){
-            System.out.println("Сегодня пятница "+Friday+"-e число. Необходимо подготовить отчет ");
+            System.out.println("7)Сегодня пятница "+Friday+"-e число. Необходимо подготовить отчет ");
             Friday += 7;
         }
         //8
         int simpleYear = 2024;
         int lastYears = simpleYear - 200;
         int FutureYears = simpleYear + 100;
-        while (lastYears <= simpleYear && simpleYear <= FutureYears){
-            if (lastYears % 79 == 0 ){
-                System.out.println(lastYears);
-                lastYears++;
-            } else if (FutureYears % 79 ==0) {
-                System.out.println(FutureYears);
-                FutureYears++;
-            } else {
-                lastYears++;
-                FutureYears++;
+        while (lastYears <= FutureYears){
+            if (lastYears % 79 == 0){
+                System.out.println("8)"+lastYears);
             }
+            lastYears++;
 
         }
     }
